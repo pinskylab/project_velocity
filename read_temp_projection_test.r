b2139d1016c33d67262f2978900ba8a57e5cbb2e
Malin Pinsky <malin.pinsky@gmail.com>
2017-03-31 10:46:17 -0400

    Maps of available data from temperature projections
    Red is missing, black is present
    @JWMorley Check out these maps. Alaska is missing from the GFDL models!
    Also, many models miss the coastal areas. Thomas will work on an
    interpolation solution. We also figured out the huge negative and
    positive numbers problem: we need to use sd(SODA)/sd(GCM) instead of
    var(SODA)/var(GCM).

--- read_temp_projection_test.r
+++ read_temp_projection_test.r
@@ -1,6 +1,6 @@
 ## Set working directory
 if(Sys.info()["nodename"] == "pinsky-macbookair"){
-	setwd('~/Documents/Rutgers/Range projections/proj_ranges/')
+	setwd('~/Documents/Rutgers/Range projections/project_velocity/')
 	projfolder = '../CEmodels_proj'
 	modfolder = '../CEModels'
 	climgridfolder <- '../data/'
@@ -52,22 +52,45 @@ pred.metric <- c('max', 'min', 'mean')
 ## then will have 2007-2100
 
 ## testing file reading on sbt data
-i=3; k=2; j=2; l=3 # sbt_rcp85, ond, mean
+i=3; k=2; j=1; l=3 # sbt_rcp85, ond, mean
 filename = paste('data/', pred.folder[i], modelrun[j], '_rcp', rcp, '_regrid.nc_2006_2100_', pred.season[k],'_', pred.metric[l], '.txt', sep="")
 filein <- readLines(filename)
 temps <- t(sapply(filein, processlines))
+row.names(temps) <- 1:nrow(temps)
 
-
+	# map the available data
+	modeldat <- apply(temps, MARGIN=1, FUN = function(x) all(!is.na(x))) # whether or not a model has data at each grid cell
+	modeldat <- cbind(modeldat, clim.grid)
+	
+	plot(modeldat$longrid, modeldat$latgrid, col=c('red', 'black')[as.numeric(modeldat$modeldat)+1], cex=0.1, main=modelrun[j])
+	
 
 ## test to read in a file from each model (sbt data)
 i=3; k=4; l=3 # sbt_rcp85, ond, mean
-temps <- array(as.numeric(NA), dim=c(13637,94,length(modelrun)), dimnames=list(grid=1:13637, year=1:94, model=1:length(modelrun)))
+temps <- array(as.numeric(NA), dim=c(13637,94,length(modelrun)), dimnames=list(grid=1:13637, year=1:94, model=modelrun))
 for(j in 1:length(modelrun)){
 	print(j)
 	filename = paste('data/', pred.folder[i], modelrun[j], '_rcp', rcp, '_regrid.nc_2006_2100_', pred.season[k],'_', pred.metric[l], '.txt', sep="")
 	filein <- readLines(filename)
 	temps[,,j] <- t(sapply(filein, processlines))
 }
+
+	# save out
+	save(temps, file='data/sbt_rcp85_mean_allmods.rdata')
+	
+	# plot available data for all
+	png(file='figures/temp_projection_data_maps.png', width=14, height=14, units='in', res=300)
+	par(mfrow=c(4,4))
+	for(j in 1:dim(temps)[3]){	
+		modeldat <- apply(temps[,,j], MARGIN=1, FUN = function(x) all(!is.na(x))) # whether or not a model has data at each grid cell
+		modeldat <- cbind(modeldat, clim.grid)
+	
+		plot(modeldat$longrid, modeldat$latgrid, col=c('red', 'black')[as.numeric(modeldat$modeldat)+1], cex=0.01, main=modelrun[j])
+	}
+	dev.off()
+	
+# what range of temperatures in each model?
+apply(temps, MARGIN=3, FUN=function(x) summary(as.numeric(x)))
 
 # how much data do we have? (need array from previous code block)
 modeldat <- apply(temps, MARGIN=c(1,3), FUN = function(x) all(!is.na(x))) # whether or not a model has data at each grid cell
